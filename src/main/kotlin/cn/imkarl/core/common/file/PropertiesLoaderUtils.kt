24216e81c45e3849426f37065b3e14160ca35eba
imkarl <imkarl@126.com>
2021-02-10 10:54:27 +0800

    added PropertiesLoaderUtils

---
+++ src/main/kotlin/cn/imkarl/core/common/file/PropertiesLoaderUtils.kt
@@ -0,0 +1,31 @@
+package cn.imkarl.core.common.file
+
+import java.io.File
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.util.*
+
+/**
+ * Properties加载工具类
+ * @author imkarl
+ */
+object PropertiesLoaderUtils {
+
+    fun loadProperties(
+        inputStream: InputStream,
+        charset: Charset = Charsets.UTF_8
+    ): Properties {
+        return Properties().apply {
+            this.load(InputStreamReader(inputStream, charset))
+        }
+    }
+
+    fun loadProperties(
+        file: File,
+        charset: Charset = Charsets.UTF_8
+    ): Properties {
+        return loadProperties(file.inputStream(), charset)
+    }
+
+}
