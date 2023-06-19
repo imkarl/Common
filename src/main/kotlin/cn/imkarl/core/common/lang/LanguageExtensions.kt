8afa487e5fc4d960810ae88940051e374c3afddc
imkarl <imkarl@126.com>
2023-06-19 14:44:52 +0800

    feature: added LanguageExtensions

---
+++ src/main/kotlin/cn/imkarl/core/common/lang/LanguageExtensions.kt
@@ -0,0 +1,24 @@
+package cn.imkarl.core.common.lang
+
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+
+fun KType.isClass(clazz: KClass<*>): Boolean = this.classifier == clazz
+
+fun KType.instanceOf(instance: Any): Boolean = this.classifier == instance::class
+
+fun Any.instanceOf(type: KType): Boolean = type.classifier == this::class
+
+
+val KType.isTypeString: Boolean get() = this.isClass(String::class)
+val KType.isTypeInt: Boolean get() = this.isClass(Int::class) || this.isClass(java.lang.Integer::class)
+val KType.isTypeLong: Boolean get() = this.isClass(Long::class) || this.isClass(java.lang.Long::class)
+val KType.isTypeByte: Boolean get() = this.isClass(Byte::class) || this.isClass(java.lang.Byte::class)
+val KType.isTypeShort: Boolean get() = this.isClass(Short::class) || this.isClass(java.lang.Short::class)
+val KType.isTypeChar: Boolean get() = this.isClass(Char::class) || this.isClass(java.lang.Character::class)
+val KType.isTypeBoolean: Boolean get() = this.isClass(Boolean::class) || this.isClass(java.lang.Boolean::class)
+val KType.isTypeFloat: Boolean get() = this.isClass(Float::class) || this.isClass(java.lang.Float::class)
+val KType.isTypeDouble: Boolean get() = this.isClass(Double::class) || this.isClass(java.lang.Double::class)
+val KType.isTypeByteArray: Boolean get() = this.isClass(ByteArray::class)
+
